9dfaf954b54bb9ae66886722520bf3737b1b6530
TheKrakenIO <149908418+TheKrakenIO@users.noreply.github.com>
2023-11-09 10:10:16 -0500

    Update theinpector.py
    1.2

--- theinpector.py
+++ theinpector.py
@@ -1,93 +1,141 @@
 import requests
-import socket
+import re
+import time
 import logging
+import socket
 from urllib.parse import urlparse
 
 # Constants
-TIMEOUT = 10
-MAX_REDIRECTS = 10  #you can change this
+TIMEOUT = 20  # Increased timeout
+MAX_RETRIES = 3  # Number of retries
+RETRY_BACKOFF = 2  # Backoff multiplier for retries
+MAX_REDIRECTS = 10  # Maximum number of redirects
 USER_AGENT = "Mozilla/5.0 (compatible; RedirectChecker/1.0; +http://example.com/bot)"
 
-# Configure logging
-logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+# Enhanced logging configuration with timestamps
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-def get_redirects(domain):
-    url = prepend_scheme(domain)
-    if not url:
-        logging.error("Failed to access the domain with both http and https.")
-        return
-
-    try:
-        with requests.Session() as session:
-            session.max_redirects = MAX_REDIRECTS
-            session.headers.update({'User-Agent': USER_AGENT})
-            response = session.get(url, timeout=TIMEOUT, allow_redirects=True)
-
-            if not response.history:
-                logging.info("No redirects occurred.")
-
-            for redirect in response.history:
-                process_response(redirect, "Redirect")
-
-            process_response(response, "Final destination")
-            response.raise_for_status()
-
-    except requests.exceptions.HTTPError as e:
-        logging.error("HTTP error: %s", e)
-        if response.status_code == 404:
-            logging.error("404 Not Found: The requested resource at %s does not exist.", response.url)
-    except requests.exceptions.ConnectionError as e:
-        logging.error("Error connecting to the server: %s", e)
-    except requests.exceptions.Timeout as e:
-        logging.error("Timeout error: %s", e)
-    except requests.exceptions.TooManyRedirects as e:
-        logging.error("Too many redirects: %s", e)
-    except requests.exceptions.RequestException as e:
-        logging.error("Error during request: %s", e)
-
+# Function to prepend http or https to the domain
 def prepend_scheme(domain):
-    for scheme in ('http://', 'https://'):
+    schemes = ['http://', 'https://']
+    for scheme in schemes:
         url = scheme + domain
         try:
             response = requests.head(url, timeout=TIMEOUT, allow_redirects=False, headers={'User-Agent': USER_AGENT})
-            if response.status_code in (301, 302, 303, 307, 308):
-                url = response.headers.get('Location', url)
-            response.raise_for_status()
-            return url
+            if response.status_code in (200, 301, 302, 303, 307, 308):
+                return url
         except requests.exceptions.RequestException as e:
-            logging.warning("Error with %s: %s", url, e)
-            continue
+            logging.warning(f"Error with {url}: {e}")
     return None
 
+# Function to process the response
 def process_response(response, label):
     url = response.url
     status_code = response.status_code
-    parsed_url = urlparse(url)
-    ip = retrieve_ip(parsed_url.hostname)
+    ip = retrieve_ip(urlparse(url).hostname)
+    logging.info(f"\n{label}:\n - URL: {url}\n - Status code: {status_code}\n - IP address: {ip}\n")
 
-    logging.info("-" * 40)
-    logging.info("%s: %s", label, url)
-    logging.info("Status code: %d", status_code)
-    logging.info("IP address: %s", ip)
 
-    # Check for a file download attempt in the response headers
-    content_disposition = response.headers.get('Content-Disposition', '')
-    if 'attachment' in content_disposition:
-        logging.warning("Warning: The URL at %s might be trying to initiate a file download.", url)
-        # Optionally, you can extract the filename from the header
-        filename = content_disposition.partition('filename=')[-1].strip()
-        if filename:
-            logging.warning("Filename suggested by the server: %s", filename)
-    
-    logging.info("-" * 40 + "\n")
-
+# Function to retrieve IP address
 def retrieve_ip(hostname):
     try:
-        return socket.gethostbyname(hostname)
+        ip = socket.gethostbyname(hostname)
+        return ip
     except socket.gaierror:
         return "IP address not found"
 
-# Main execution
+# Check server availability
+def check_server_availability(ip, port, timeout=10):
+    """Check the server availability by attempting to connect using a socket."""
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(timeout)
+    try:
+        result = sock.connect_ex((ip, port))
+        if result == 0:
+            logging.info(f"Server {ip} is responding on port {port}.")
+            return True
+        else:
+            logging.warning(f"Server {ip} is not responding on port {port}.")
+            return False
+    except socket.error as err:
+        logging.error(f"Socket error: {err}")
+        return False
+    finally:
+        sock.close()
+
+# Retry mechanism for HTTP requests
+def make_request_with_retries(url, session):
+    parsed_url = urlparse(url)
+    port = 80 if parsed_url.scheme == "http" else 443
+    for i in range(MAX_RETRIES):
+        if not check_server_availability(parsed_url.hostname, port):
+            logging.warning(f"The server at {parsed_url.netloc} may be down or experiencing issues.")
+            time.sleep(RETRY_BACKOFF ** i)
+            continue
+        try:
+            response = session.get(url, timeout=TIMEOUT, allow_redirects=True)
+            response.raise_for_status()
+            return response
+        except requests.exceptions.RequestException as e:
+            wait = RETRY_BACKOFF ** i
+            logging.warning(f"Attempt {i+1}/{MAX_RETRIES} failed: {e}. Retrying in {wait} seconds...")
+            time.sleep(wait)
+    logging.error("Failed to connect to the domain after all retries. Please check the domain name and try again.")
+    return None
+
+# User input validation
+def validate_domain(domain):
+    pattern = r"^(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$"
+    if re.match(pattern, domain):
+        return True
+    else:
+        logging.error("Invalid domain format. Please enter a valid domain.")
+        return False
+
+# DNS resolution check
+def check_dns_resolution(domain):
+    try:
+        ip = socket.gethostbyname(domain)
+        logging.info(f"DNS resolution successful: {domain} resolved to {ip}")
+        return True
+    except socket.gaierror:
+        logging.error(f"DNS resolution failed for domain: {domain}")
+        return False
+
+# Main function that gets redirects
+def get_redirects(domain):
+    url = prepend_scheme(domain)
+    if not url:
+        logging.error("Failed to access the domain with both HTTP and HTTPS.")
+        return
+    with requests.Session() as session:
+        session.max_redirects = MAX_REDIRECTS
+        session.headers.update({'User-Agent': USER_AGENT})
+        response = make_request_with_retries(url, session)
+        if response is None:
+            return
+        if not response.history:
+            logging.info("No redirects occurred.")
+        else:
+            logging.info(f"Redirects for {domain}:")
+        for redirect in response.history:
+            process_response(redirect, "Redirect")
+        process_response(response, "Final Destination")
+
+# Main execution with input validation and DNS check
+def main():
+    try:
+        domain = input("Enter the domain: ")
+        if validate_domain(domain) and check_dns_resolution(domain):
+            print(f"Checking redirects for {domain}...\n")
+            get_redirects(domain)
+        else:
+            print("Please check the domain name and try again.")
+    except Exception as e:
+        logging.error(f"An unexpected error occurred: {e}")
+    finally:
+        print("\nThanks and good luck on the hunt!")
+
+
 if __name__ == "__main__":
-    domain = input("Enter the domain: ")
-    get_redirects(domain)
+    main()
